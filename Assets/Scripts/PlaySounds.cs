7648b2dd2a1f21ea617ac7239ce3137b0dc76a40
SATSM <fons.tewinkel@student.hku.nl>
2018-05-16 10:45:21 +0200

    Only needs testing
    And fore and mid sounds.
    And graphics probs...

--- Assets/Scripts/PlaySounds.cs
+++ Assets/Scripts/PlaySounds.cs
@@ -5,13 +5,24 @@ using UnityEngine.UI;
 
 public class PlaySounds : MonoBehaviour {
 	int[] currentTracks = new int[3];
+	AudioSource playerFore;
+	AudioSource playerMid;
+	AudioSource playerBack;
 
-	public AudioSource[] fore = new AudioSource[6];
-	public AudioSource[] mid  = new AudioSource[6];
-	public AudioSource[] back = new AudioSource[6];
+	public AudioClip[] fore = new AudioClip[6];
+	public AudioClip[] mid  = new AudioClip[6];
+	public AudioClip[] back = new AudioClip[6];
 
 	// Use this for initialization
 	void Start () {
+		AudioSource[] allplayers = GetComponents<AudioSource> ();
+
+		if (allplayers.Length >= 3) {
+			playerFore = allplayers [0];
+			playerMid  = allplayers [1];
+			playerBack = allplayers [2];
+		} else
+			print ("The button needs at least 3 audio sources to function.");
 	}
 	
 	// Update is called once per frame
@@ -22,10 +33,10 @@ public class PlaySounds : MonoBehaviour {
 	public void PlayRandomMusic()
 	{
 		for (int t = 0; t < 3; t++)
-			currentTracks[t] = Random.Range (0, 5);
+			currentTracks[t] = Random.Range (0, 2);
 
-		fore [currentTracks [0]].Play ();
-		mid  [currentTracks [1]].Play ();
-		back [currentTracks [2]].Play ();
+		playerFore.PlayOneShot (fore [currentTracks [0]]);
+		playerMid.PlayOneShot  (mid  [currentTracks [1]]);
+		playerBack.PlayOneShot (back [currentTracks [2]]);
 	}
 }
